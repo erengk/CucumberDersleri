96d8479d31c85de755d8ad4bd2c6115df5b57098
Gökhan Eren Kamer <gek@eskisehir.edu.tr>
2023-05-03 19:37:21 +0300

    Hooks değişti

--- Cucumber_Kursu5/src/test/java/StepDefinitions/Hooks.java
+++ Cucumber_Kursu5/src/test/java/StepDefinitions/Hooks.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Utilities.ExcelUtility;
 import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
@@ -11,6 +12,9 @@ public class Hooks {
     @After// her senaryodan sonra çalışacak bölüm
     public void after(Scenario senaryo){
         System.out.println("Senaryo Bitti");
+
+        ExcelUtility.writeExcel("src/test/java/ApachePOI/resource/ScenarioStatus.xlsx",
+                senaryo, GWD.threadBrowserGet());
 
         if (senaryo.isFailed()){
             TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
